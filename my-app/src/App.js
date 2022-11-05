65b0bb934079a2249c2adcfcb8a28bf3331340de
Andrei Grusheuski <gruand13@gmail.com>
2022-11-05 13:01:16 +0300

    work with state lesson 130

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,36 @@
-
+import {Component} from "react";
 import './App.css';
 
 
 // use destructurizatin props object
-function WhoAmI ({name, surname, link}){
-    return (
-        <div>
-            {/* if object name.first name */}
-            <h1>My name is {name()}, surname - {surname}</h1>
-            <a href={link}>My profile</a>
-        </div>
-    )
+class WhoAmI extends Component{
+    constructor (props){
+        super(props);
+        this.state= {
+            years: 37,
+            text: "++++"
+        }
+    }
+
+    nextYear= ()=>{
+        console.log("+++");
+        this.setState(state =>({
+            years: state.years +1 
+        }))
+    }
+
+    render (){
+        const {name, surname, link} = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                {/* if object name.firstName */}
+                <h1>My name is {name}, surname - {surname}, age-{this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+    
 }
 
 
@@ -18,8 +38,8 @@ function App() {
   return (
     <div className="App">
         {/* if object firstName: "Alex" */}
-        <WhoAmI name={()=> {return "John"}} surname="Hrusheuski" link="facebook.com"/>
-        <WhoAmI name={()=> {return "John"}} surname="Pupkin" link="vk.com"/>
+        <WhoAmI name="Johnny" surname="Hrusheuski" link="facebook.com"/>
+        <WhoAmI name="John" surname="Pupkin" link="vk.com"/>
     </div>
   );
 }
