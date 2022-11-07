766ce339586849b28672ee4e0dbe83450290f5b4
Andrei Grusheuski <gruand13@gmail.com>
2022-11-07 09:41:54 +0300

    add Fragment(another variant

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -34,7 +34,7 @@ class WhoAmI extends Component{
         const {name, surname, link} = this.props;
         const {position, years} = this.state;
         return (
-            <Fragment>
+            <>
                 <button onClick={this.nextYear}>++++</button>
                 {/* if object name.firstName */}
                 <h1>My name is {name}, surname - {surname},
@@ -45,7 +45,7 @@ class WhoAmI extends Component{
                     <span>Enter</span>
                     <input type="text" onChange={(e)=>this.commitInputChanges(e, 'some color')}/>
                 </form>
-            </Fragment>
+            </>
         )
     }
     
